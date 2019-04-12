e40a729b8ffe13b342f28eb2ad46d4a248bac7ac
Jos <jos@promidata.com>
2019-04-12 10:39:44 +0200

    Project first commit. Added the model, client and api projects from ynab-dotnet.

---
+++ src/YNAB.Statistics/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Forms;
+using YNAB.Api;
+using YNAB.Client;
+
+namespace YNAB.Statistics
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Configure API key authorization: bearer
+            Configuration.ApiKey.Add("Authorization", "YOUR_API_KEY_HERE");
+            // Setup prefix (e.g. Bearer) for API key
+            Configuration.ApiKeyPrefix.Add("Authorization", "Bearer");
+
+            BudgetsApi budgetApi = new BudgetsApi();
+            var budgets = budgetApi.GetBudgets();
+
+        }
+    }
+}
